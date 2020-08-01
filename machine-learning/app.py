d155b9528a701b067abb69586a379bca8a0eaada
shreyagupta30 <shreyagupta3011@gmail.com>
2020-08-01 16:34:50 +0530

    add ml-models

---
+++ machine-learning/app.py
@@ -0,0 +1,35 @@
+import logging
+import os
+from predict import predict_main
+import requests
+
+from flask import Flask, request, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    # print(request.json)
+    response_get = requests.get(request.json.get('image'))
+    try:
+        file = open("index.jpeg", "wb")
+        file.write(response_get.content)
+        file.close()
+        food_class = predict_main()
+        os.remove('index.jpeg')
+        return food_class
+    
+    except Exception:
+        os.remove('index.jpeg')
+        return "Error in parsing"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='5000')
