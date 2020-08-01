8f353ef66e825e545735d41e905698b2c1f5e926
Kush Daga <kushdaga@hotmail.com>
2020-08-01 14:47:46 +0530

    Completed Login Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from '';
+import Login from './pages/Login';
 
 function App() {
   return <Login />;
---
+++ src/pages/Login/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+`;
+export const SubContainer = styled.div`
+  display: flex;
+  width: 40vw;
+  justify-content: space-between;
+  align-items: center;
+  background: #f3f3f3;
+  padding: 50px;
+`;
+
+export const Text = styled.div`
+  h1 {
+    font-weight: 900;
+    text-transform: uppercase;
+    margin: 0;
+  }
+`;
