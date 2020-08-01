ddfd2a9c0ff41b935f6843c2f17755415582503f
Jay Parmani <jsparmani@gmail.com>
2020-08-01 10:46:45 +0530

    Add frontend start code and react backend binding

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+catchall = TemplateView.as_view(template_name='index.html')
 
 urlpatterns = [
+    path('', catchall),
     path('admin/', admin.site.urls),
 ]
 
