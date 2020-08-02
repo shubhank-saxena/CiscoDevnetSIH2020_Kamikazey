462d6a3b37c785e5b6eee360ca0b4bfbc770bffa
Jay Parmani <jsparmani@gmail.com>
2020-08-02 10:45:28 +0530

    snapshot api

--- backend/food_detection/api/urls.py
+++ backend/food_detection/api/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
-from .views import FoodDetectionView
+from .views import FoodDetectionView, meraki_snapshot
 
-urlpatterns = [path('predict/', FoodDetectionView.as_view())]
+urlpatterns = [path('predict/', FoodDetectionView.as_view()), path('snapshot/', meraki_snapshot)]
